b74e04db7eab8161b5d53c26d7478f25783f354f
Satish Surath <s@n-m.co>
2022-04-05 12:58:53 -0400

    Adding Loops and Conditional statements to Flask Template HTML
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Group 11'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Toronto!'
-        },
-        {
-            'author': {'username': 'Jane'},
-            'body': 'Python Class was so cool!'
-        }
-    ]
+    posts = [{'author': {'username': 'John'},'body': 'Beautiful day in Toronto!'},{'author': {'username': 'Jane'},'body': 'Python Class was so cool!'}]
     return render_template('index.html', title='Home', user=user)
 
 
