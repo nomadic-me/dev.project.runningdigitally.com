b1ea6f2c6e30d9df814b736ff7a5a5d460e3e45f
Satish Surath <s@n-m.co>
2022-04-05 16:37:45 -0400

    Adding Flask Database Support
    Signed-off-by: Satish Surath <s@n-m.co>

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'mysql://dev_prj_rnigdigi:ePC-Xq9MePC-Xq9M@mysql.dev.runningdigitally.com/dev_project_runningdigitally'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
