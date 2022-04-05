c4152866646dff165f92533412e371db733b51e3
Satish Surath <s@n-m.co>
2022-04-05 16:13:44 -0400

    Debugging Login forms
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -11,12 +11,13 @@ def index():
     posts = [{'author': {'username': 'John'},'body': 'Beautiful day in Toronto!'},{'author': {'username': 'Jane'},'body': 'Python Class was so cool!'}]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    user = {'username': 'Group 11'}
+    posts = [{'author': {'username': 'John'},'body': 'Beautiful day in Toronto!'},{'author': {'username': 'Jane'},'body': 'Python Class was so cool!'}]
+    return render_template('index.html', title='Form - Login', user=user, posts=posts)
+    #if form.validate_on_submit():
+     #   flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+      #  return redirect(url_for('index'))
+    #return render_template('login.html', title='Sign In', form=form)
