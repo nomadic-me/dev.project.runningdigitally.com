4a81b5d3572b3dda6ff241919fd8202d1627340a
dh_5ejt8t <dh_5ejt8t@gilmer.dreamhost.com>
2022-04-05 06:30:48 -0700

    Initial commit with Hello World
    Signed-off-by: dh_5ejt8t <dh_5ejt8t@gilmer.dreamhost.com>

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import sys, os
+INTERP = os.path.join(os.environ['HOME'], 'dev.project.runningdigitally.com', 'venv2', 'bin', 'python3')
+if sys.executable != INTERP:
+        os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+
+from flask import Flask
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return 'Hello from Passenger (and Fintech Group 11), - Something awesome is brewing! Standby for more updates!'
