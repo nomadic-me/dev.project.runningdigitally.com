450bd38cc7fe96a1e2f2e21448b404aff94e0233
Satish Surath <s@n-m.co>
2022-04-06 09:52:03 -0400

    adding Shell Context
    Signed-off-by: Satish Surath <s@n-m.co>

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -15,3 +15,10 @@ application = Flask(__name__)
 
 sys.path.append('app')
 from app import app as application
+
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
