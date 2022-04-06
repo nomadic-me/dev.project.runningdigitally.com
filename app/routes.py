51bb720af37301c51b837e9622e226423b16f592
Satish Surath <s@n-m.co>
2022-04-06 18:18:07 -0400

    Moving email.py to correct folder
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -157,8 +157,7 @@ def reset_password_request():
             send_password_reset_email(user)
         flash('Check your email for the instructions to reset your password')
         return redirect(url_for('login'))
-    return render_template('reset_password_request.html',
-                           title='Reset Password', form=form)
+    return render_template('reset_password_request.html',title='Reset Password', form=form)
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
