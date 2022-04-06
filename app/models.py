6073f57a73c07361629b86f23171f8834aab6762
Satish Surath <s@n-m.co>
2022-04-06 11:23:37 -0400

    Fixing and debugging issues - internal server error
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/models.py
+++ app/models.py
@@ -6,9 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +14,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -24,6 +23,10 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
-from flask import render_template
-from app import app
-from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user
-from flask_login import logout_user
-from flask_login import login_required
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app import app, db
 from app.models import User
 from flask import request
-from werkzeug.urls import url_parse
+
+
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -25,7 +25,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password - Are you really sure you know what you are doing?')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
