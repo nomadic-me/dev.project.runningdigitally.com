a10b3be88683a3537b5680de67c14c8bba71a202
Satish Surath <s@n-m.co>
2022-04-06 08:32:05 -0400

    Copied .flaskenv to .env and added load_dotenv() in config.py
    Signed-off-by: Satish Surath <s@n-m.co>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
