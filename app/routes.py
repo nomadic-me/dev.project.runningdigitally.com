0bf5ac38c5b8ccc5b39dd7caa413f24b2c0a905c
Satish Surath <s@n-m.co>
2022-04-06 13:48:32 -0400

    Adding User Profile Link
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app import app, db
 from app.models import User
 from flask import request
 
-
+from app.forms import RegistrationForm
 from app.forms import LoginForm
 
 
@@ -38,3 +38,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
