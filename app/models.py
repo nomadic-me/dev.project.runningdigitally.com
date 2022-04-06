b49b9ad2c9ef2a5117d3d25f4c76f1441aebb322
Satish Surath <s@n-m.co>
2022-04-06 15:52:54 -0400

    Adding Followers in model.py
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/models.py
+++ app/models.py
@@ -7,6 +7,12 @@ from hashlib import md5
 
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -49,8 +55,3 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
-    
