b8b5c5c362ff9cef8e8a42f618199a97c77540c9
Satish Surath <s@n-m.co>
2022-04-06 16:34:02 -0400

    Adding Blog Posting Feature along with Explore feature
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,8 @@ from app.forms import RegistrationForm
 from app.forms import LoginForm
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
 
 @app.before_request
@@ -23,8 +25,16 @@ def before_request():
 @app.route('/index')
 @login_required
 def index():
-    posts = [{'author': {'username': 'John'},'body': 'Beautiful day in Toronto!'},{'author': {'username': 'Jane'},'body': 'Python Class was so cool!'}]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    #posts = [{'author': {'username': 'John'},'body': 'Beautiful day in Toronto!'},{'author': {'username': 'Jane'},'body': 'Python Class was so cool!'}]
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home', form=form,posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -127,3 +137,9 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
