05da3440b56ae66856c82ce787ebc20aad590328
Satish Surath <s@n-m.co>
2022-04-06 19:50:17 -0400

    ------Wrapping up Base Level Application
    Signed-off-by: Satish Surath <s@n-m.co>

--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['s@n-m.co']
+    ADMINS = ['info@runningdigitally.com']
+    LANGUAGES = ['en', 'es']
