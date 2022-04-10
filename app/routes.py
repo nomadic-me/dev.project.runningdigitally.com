1938d627dbc972b35259b25c6bcaa45623b10560
Satish Surath <s@n-m.co>
2022-04-10 17:48:54 -0400

    fixing issue with graphJSON variable name
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -41,8 +41,8 @@ def graph1():
     'Amount': [4, 1, 2, 2, 4, 5],
     'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
     })
-    fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
-    graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    fig1 = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
+    graphJSON1 = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('graph.html', graphJSON=graphJSON1, title='Graph')
         
     
