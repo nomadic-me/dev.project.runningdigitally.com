e391812c1083b8036f5370c930dbdca5384ea499
Satish Surath <s@n-m.co>
2022-04-10 16:32:37 -0400

    Adding a simple Plotly (not Dash) content
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -17,9 +17,30 @@ from app.forms import ResetPasswordRequestForm
 from app.email import send_password_reset_email
 from app.forms import ResetPasswordForm
 
+#Adding simple Plotly Graph
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
         
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')        
+    return render_template('index.html', title='Home')
+
+#Adding simple Plotly Graph
+@app.route('/Graph')
+    def notdash():
+    df = pd.DataFrame({
+      'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
+      'Amount': [4, 1, 2, 2, 4, 5],
+      'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    })
+    fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('graph.html', graphJSON=graphJSON, title='Graph')
+        
+
+
 
