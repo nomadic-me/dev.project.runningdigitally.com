73a3823af4d0b24a6956f03ccc1ae47a856a4e0c
Satish Surath <s@n-m.co>
2022-07-05 18:20:33 -0400

    refactoring and cleaning up unused code
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,9 @@
 from flask import render_template, flash, redirect, url_for
-#from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-
 from app import app
-#from app import app, db
-#from app.models import User
 from flask import request
 from datetime import datetime
 
-#from app.forms import RegistrationForm
-#from app.forms import LoginForm
-#from app.forms import EditProfileForm
-#from app.forms import EmptyForm
-#from app.forms import PostForm
-#from app.models import Post
-#from app.forms import ResetPasswordRequestForm
-#from app.email import send_password_reset_email
-#from app.forms import ResetPasswordForm
 
 #Adding simple Plotly Graph
 import pandas as pd
@@ -32,6 +19,7 @@ import yfinance as yf
 #Adding Alpaca API Imports
 import os
 import requests
+
 #Importing MCForecast Tool
 from MCForecastTools import MCSimulation
 
@@ -58,13 +46,6 @@ def cb():
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
-
-#@app.route('/new')
-#def new():
-#    return render_template('new.html', title='New')
-    
-    
-
     
 @app.route('/callback2/<endpoint>')
 def cb2(endpoint):   
